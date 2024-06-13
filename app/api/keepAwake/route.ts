178290966749e9c8bc976d3ac9a522a9c79120ec
Chukwu Matthew <codemat.biz@gmail.com>
2024-06-13 21:31:16 +0100

    added cron jobs

--- app/api/keepAwake/route.ts
+++ app/api/keepAwake/route.ts
@@ -1,30 +1,44 @@
-// app/api/keepAwake/route.ts
+// // app/api/keepAwake/route.ts
 
-import { createClient } from "@supabase/supabase-js";
-import { NextRequest, NextResponse } from "next/server";
+// import { createClient } from "@supabase/supabase-js";
+// import { NextRequest, NextResponse } from "next/server";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-const cronSecret = process.env.CRON_SECRET as string;
+// const supabase = createClient(supabaseUrl, supabaseKey);
+// const cronSecret = process.env.CRON_SECRET as string;
 
-export async function GET(req: NextRequest) {
-  const url = new URL(req.url);
-  const secret = req.headers.get("x-cron-secret");
-//   if (secret !== cronSecret) {
-//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+// export async function GET(req: NextRequest) {
+//   const url = new URL(req.url);
+//   const secret = req.headers.get("x-cron-secret");
+// //   if (secret !== cronSecret) {
+// //     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+// //   }
+
+//   try {
+//     const { data, error } = await supabase.from("records").select("*").limit(1);
+
+//     if (error) {
+//       throw error;
+//     }
+
+//     return NextResponse.json({ message: "Supabase is awake", data });
+//   } catch (error: any) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
 //   }
+// }
 
-  try {
-    const { data, error } = await supabase.from("records").select("*").limit(1);
 
-    if (error) {
-      throw error;
+
+export async function GET() {
+  const result = await fetch(
+    "http://worldtimeapi.org/api/timezone/America/Chicago",
+    {
+      cache: "no-store",
     }
+  );
+  const data = await result.json();
 
-    return NextResponse.json({ message: "Supabase is awake", data });
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+  return Response.json({ datetime: data.datetime });
 }
